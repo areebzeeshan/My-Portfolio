c76304171d72cb7535929d9936f7fc0ff60667e5
areebzeeshan <areebzeeshan03@gmail.com>
2024-07-07 18:01:49 +0500

    expense teacker added and ui updated

---
+++ src/data/data.js
@@ -0,0 +1,63 @@
+import gifSharing from '../assets/images/gif-sharing.png';
+import multisite from '../assets/images/multisite.png'
+import NFT2 from '../assets/images/nft-2.png'
+import nftFrontend from '../assets/images/nft-frontend.png'
+import presaleNFT from '../assets/images/presale-nft.png'
+import simulation from '../assets/images/simulation.png'
+import tenderely from '../assets/images/tenderely.png'
+import trabajo from '../assets/images/trabajo.png'
+import eliteBond from '../assets/images/elite-bond.png'
+import expenseTracker from '../assets/images/dashboard.png'
+
+export const projects = [
+  {
+    imgSrc: multisite,
+    title: "Multisite",
+    url: "https://multisite-eight.vercel.app/",
+  },
+  {
+    imgSrc: presaleNFT,
+    title: "Presale NFT",
+    url: "https://presale-nft-five.vercel.app/",
+  },
+  {
+    imgSrc: expenseTracker,
+    title: "Expense Tracker",
+    url: "https://expense-tracker-lake-theta.vercel.app/",
+  },
+  {
+    imgSrc: simulation,
+    title: "Simulations",
+    url: "https://simulation-project-xi.vercel.app/",
+  },
+  {
+    imgSrc: eliteBond,
+    title: "Elite Bond 2.0",
+    url: "https://elite2-0.vercel.app/",
+  },
+  {
+    imgSrc: nftFrontend,
+    title: "NFT (Landing Page)",
+    url: "https://nft-frontend-gold-psi.vercel.app/",
+  },
+  {
+    imgSrc: tenderely,
+    title: "Tenderely",
+    url: "https://tenderely.vercel.app/",
+  },
+  {
+    imgSrc: trabajo,
+    title: "Trabajo",
+    url: "https://trabajo-ruby.vercel.app/",
+  },
+  {
+    imgSrc: gifSharing,
+    title: "GIF Sharing",
+    url: "https://gif-sharing-lac.vercel.app/",
+  },
+  {
+    imgSrc: NFT2,
+    title: "Voldemort NFTs",
+    url: "https://nft-2-flame.vercel.app/",
+  },
+];

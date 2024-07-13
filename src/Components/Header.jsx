f2815df12b6d008630d0ff71ab67761d7a677249
Areeb Zeeshan <88983748+areebzeeshan@users.noreply.github.com>
2024-07-13 13:30:55 +0500

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -12,18 +12,18 @@ const Header = () => {
          <section className='adjust items-center'>
             <h1 className='text-white text-5xl leading-snug font-bold'>Hi, I'm <span>Areeb Zeeshan</span></h1>
             <div className='text-animate text-4xl'>
-                <h3>Frontend Developer</h3>
+                <h3>MERN Stack Developer</h3>
             </div>
             <p className='text-white w-100 md:w-9/12 py-6'>
-                🚀 Hi, I'm Areeb, a passionate frontend developer with a keen eye for clean and intuitive user interfaces.
-                Leveraging my expertise in HTML, CSS, and JavaScript, I transform ideas into engaging and responsive web experiences.
+                🚀 Hi, I'm Areeb, a passionate mern stack developer with a keen eye for clean and intuitive user interfaces.
+                Leveraging my expertise in React JS, Redux, Next JS, Express JS, MongoDB and much more. I transform ideas into engaging and responsive web experiences.
                 From crafting pixel-perfect designs to embracing the latest frontend technologies, I thrive on bringing creativity and
                 functionality together to deliver seamless user interactions. Let's build something amazing together! ✨
             </p>
-            <div className="grid gap-3 md:flex md:space-x-2">
-                <button className="bg-[#00A1E4] hover:bg-transparent hover:text-[#00A1E4] hover:outline text-white font-bold py-2 px-4 rounded transition">
+            <div href="" className="grid gap-3 md:flex md:space-x-2">
+                <a className="bg-[#00A1E4] hover:bg-transparent hover:text-[#00A1E4] hover:outline text-white font-bold py-2 px-4 rounded transition">
                     Hire Me
-                </button>
+                </a>
                 <button className="bg-transparent outline outline-offset-1 outline-1 hover:bg-[#00A1E4] hover:text-white hover:outline-none text-[#00A1E4] font-bold py-2 px-4 rounded">
                     Let's Talk
                 </button>
